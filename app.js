ffaf1744b02fc1cf1a51f32a1a743c2c287867aa
keithchasen <keithchasen89@gmail.com>
2020-02-07 23:57:30 +0200

    add product and error views using pug

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'))
+    res.status(404).render('error', { title: 'Page not found'})
 })
 
 app.listen(3000)
