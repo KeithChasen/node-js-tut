bd5551f0ed150d6f0007b880193f411e39e9987b
keithchasen <keithchasen89@gmail.com>
2020-02-07 17:26:54 +0200

    sharing data accross requests

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 const app = express()
 
-const adminRoutes = require('./routes/admin')
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes)
+app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
